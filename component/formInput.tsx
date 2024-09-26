41f5652131849ed93ae2eaabb24989bbf4f79980
KangPhilJoong <114045849+CodeBlue0@users.noreply.github.com>
2024-09-26 10:45:47 +0000

    chore: tailwind 적용 에러 실험 위해 새로운 환경으로 이동

---
+++ component/formInput.tsx
@@ -0,0 +1,28 @@
+import { InputHTMLAttributes } from "react";
+
+interface IParams {
+  name: string;
+  errors?: string[];
+}
+
+export default function FormInput({
+  name,
+  errors = [],
+  ...rest
+}: IParams & InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <div className="w-60 p-1 border border-gray-200 rounded-2xl flex items-center">
+      <div className="*:size-5 *:fill-gray-500 p-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+        >
+          <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+          <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+        </svg>
+      </div>
+      <input className="outline-none" {...rest} />
+    </div>
+  );
+}
